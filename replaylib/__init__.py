282c25ed57ea1ded6af07a8f75ec653aa9d039c3
Scott Torborg <scott@crookedmedia.com>
2010-06-13 16:13:21 -0700

    added file handling, tweaked interface a bit, started main test

--- replaylib/__init__.py
+++ replaylib/__init__.py
@@ -1,5 +1,17 @@
 import httplib
+import pickle
 
+"""
+Replay files will be stored as a pickled dict that looks like:
+
+    {'<sha1 of canonicalized request data>': ['first response body',
+                                              'second response body',
+                                              'third response body',
+                                              ...],
+     '<sha1 of different request data>': ['first response body',
+                                          'second response body']}
+"""
+current = {}
 
 record = False
 playback = False
@@ -9,26 +21,35 @@ class StateError(Exception):
     pass
 
 
-def start_record(fname):
+def start_record():
     """
     Install an httplib wrapper that records but does not modify calls.
     """
-    global record, playback
+    global record, playback, current
     if record:
         raise StateError("Already recording.")
     if playback:
         raise StateError("Currently playing back.")
     record = True
 
+    # Install recording httplib stub.
 
-def stop_record():
+
+def stop_record(fname):
     """
-    Close the current recording and return httplib to its normal state.
+    Close the current recording, return httplib to its normal state, and save
+    the recording to fname.
     """
-    global record
+    global record, current
     if not record:
         raise StateError("Not currently recording.")
     record = False
+
+    with open(fname, 'w') as f:
+        pickle.dump(current, f)
+    current = {}
+
+    # Return httplib to original state.
 
 
 def start_playback(fname):
@@ -36,17 +57,26 @@ def start_playback(fname):
     Install an httplib wrapper that intercepts calls and returns response as
     based on the recording being played back from.
     """
-    global record, playback
+    global record, playback, current
     if record:
         raise StateError("Currently recording.")
     if playback:
         raise StateError("Already playing back.")
+    playback = True
+
+    with open(fname, 'r') as f:
+        current = pickle.load(f)
+
+    # Install playback httplib stub.
 
 
 def stop_playback():
     """
     Stop intercepting calls and return httplib to its normal state.
     """
-    global playback
+    global playback, current
     if not playback:
         raise StateError("Not currently playing back.")
+    current = {}
+
+    # Return httplib to original state.
--- replaylib/tests/__init__.py
+++ replaylib/tests/__init__.py
@@ -2,13 +2,32 @@ from unittest import TestCase
 import replaylib
 
 
-class StateTest(TestCase):
+class ReplayTest(TestCase):
+
+    def tearDown(self):
+        try:
+            replaylib.stop_record('/tmp/trash.pkl')
+        except replaylib.StateError:
+            pass
     
     def test_already_recording(self):
-        replaylib.start_record('somefile.pkl')
+        replaylib.start_record()
         try:
-            replaylib.start_record('blah.pkl')
+            replaylib.start_record()
         except replaylib.StateError:
             pass
         else:
             raise AssertionError("trying to record twice should fail")
+
+    def test_full(self):
+        replaylib.start_record()
+
+        # Do some httplib or urllib shit here.
+
+        replaylib.stop_record('/tmp/replaylib-test.pkl')
+
+        replaylib.start_playback('/tmp/replaylib-test.pkl')
+
+        # Do the same httplib shit again.
+
+        replaylib.stop_playback()
