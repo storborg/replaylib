7733b5a3bf42179f573ac8fcaf837e0659f84dad
Scott Torborg <scott@crookedmedia.com>
2010-06-14 01:36:49 -0700

    added SSL test which will run if pyOpenSSL is installed

--- replaylib/tests/__init__.py
+++ replaylib/tests/__init__.py
@@ -1,23 +1,42 @@
 from unittest import TestCase
+
+import os
+import os.path
 import urllib
 import httplib
+import socket
+
+from SocketServer import BaseServer
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 from threading import Thread
 
+try:
+    from OpenSSL import SSL
+except ImportError:
+    SSL = None
+
 import replaylib
+
+__here__ = os.path.dirname(__file__)
 
 # Port number to use for reference webserver.
 PORT = 9000
+SECURE_PORT = PORT + 1
 
 # Filename to use for pickle test.
 TEST_FILENAME = '/tmp/replaylib-test.pkl'
 
+# Private key and certificate file to use for SSL testing.
+TEST_CERT = os.path.join(__here__, 'host.pem')
+
 
 class ReferenceServer(Thread):
 
-    def __init__(self):
+    def __init__(self, port, server_class, secure):
         Thread.__init__(self)
+        self.port = port
         self.counter = 0
+        self.server_class = server_class
 
         class _Handler(BaseHTTPRequestHandler):
 
@@ -42,18 +61,24 @@ class ReferenceServer(Thread):
                 resp = '%d reqs to %s with %s' % (self.counter, s.path, body)
                 s.wfile.write(resp)
 
-            def log_message(self, *args):
+            def log_message(s, *args):
                 pass
+
+            if secure:
+                def setup(s):
+                    s.connection = s.request
+                    s.rfile = socket._fileobject(s.request, 'rb', s.rbufsize)
+                    s.wfile = socket._fileobject(s.request, 'rb', s.wbufsize)
 
         self.handler = _Handler
 
     def run(self):
-        httpd = HTTPServer(('localhost', PORT), self.handler)
+        httpd = self.server_class(('localhost', self.port), self.handler)
         httpd.serve_forever()
 
 
-server = ReferenceServer()
-server.start()
+http_server = ReferenceServer(PORT, HTTPServer, secure=False)
+http_server.start()
 
 
 class ReplayFunctionalityTest(TestCase):
@@ -293,3 +318,39 @@ class ReplayStateTest(TestCase):
             pass
         else:
             raise AssertionError("record during playback should fail")
+
+
+if SSL:
+    class SecureHTTPServer(HTTPServer):
+        def __init__(self, server_address, HandlerClass):
+            BaseServer.__init__(self, server_address, HandlerClass)
+            ctx = SSL.Context(SSL.SSLv23_METHOD)
+            ctx.use_privatekey_file (TEST_CERT)
+            ctx.use_certificate_file(TEST_CERT)
+            self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
+                                                            self.socket_type))
+            self.server_bind()
+            self.server_activate()
+
+    https_server = ReferenceServer(SECURE_PORT, SecureHTTPServer, secure=True)
+    https_server.start()
+
+    class ReplayFunctionalitySSLTest(TestCase):
+
+        def tearDown(self):
+            replaylib.reset()
+
+        def test_single(self):
+            replaylib.start_record()
+            webf = urllib.urlopen('https://localhost:%d' % SECURE_PORT)
+            real_compare = webf.read()
+            webf.close()
+            data = replaylib.stop_record_obj()
+
+            replaylib.start_playback_obj(data)
+            webf = urllib.urlopen('https://localhost:%d' % SECURE_PORT)
+            fake_compare = webf.read()
+            webf.close()
+            replaylib.stop_playback()
+
+            assert real_compare == fake_compare
