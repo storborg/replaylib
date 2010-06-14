ef5fe725c25d8525b20640fa0f6d0b1e6cee3af2
Scott Torborg <scott@crookedmedia.com>
2010-06-14 01:45:21 -0700

    move test server initialization into separate module

--- replaylib/tests/__init__.py
+++ replaylib/tests/__init__.py
@@ -1,84 +1,14 @@
 from unittest import TestCase
 
-import os
-import os.path
 import urllib
 import httplib
-import socket
 
-from SocketServer import BaseServer
-from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
-from threading import Thread
-
-try:
-    from OpenSSL import SSL
-except ImportError:
-    SSL = None
+from . import servers
 
 import replaylib
 
-__here__ = os.path.dirname(__file__)
-
-# Port number to use for reference webserver.
-PORT = 9000
-SECURE_PORT = PORT + 1
-
 # Filename to use for pickle test.
 TEST_FILENAME = '/tmp/replaylib-test.pkl'
-
-# Private key and certificate file to use for SSL testing.
-TEST_CERT = os.path.join(__here__, 'host.pem')
-
-
-class ReferenceServer(Thread):
-
-    def __init__(self, port, server_class, secure):
-        Thread.__init__(self)
-        self.port = port
-        self.counter = 0
-        self.server_class = server_class
-
-        class _Handler(BaseHTTPRequestHandler):
-
-            def do_GET(s):
-                self.counter += 1
-                s.send_response(200)
-                s.send_header('Content-type', 'text/plain')
-                s.end_headers()
-                resp = '%d requests to %s' % (self.counter, s.path)
-                s.wfile.write(resp)
-
-            def do_POST(s):
-                self.counter += 1
-                s.send_response(200)
-                s.send_header('Content-type', 'text/plain')
-                s.end_headers()
-                content_length = s.headers.getheader('Content-length')
-                if content_length:
-                    body = s.rfile.read(int(content_length))
-                else:
-                    body = ''
-                resp = '%d reqs to %s with %s' % (self.counter, s.path, body)
-                s.wfile.write(resp)
-
-            def log_message(s, *args):
-                pass
-
-            if secure:
-                def setup(s):
-                    s.connection = s.request
-                    s.rfile = socket._fileobject(s.request, 'rb', s.rbufsize)
-                    s.wfile = socket._fileobject(s.request, 'rb', s.wbufsize)
-
-        self.handler = _Handler
-
-    def run(self):
-        httpd = self.server_class(('localhost', self.port), self.handler)
-        httpd.serve_forever()
-
-
-http_server = ReferenceServer(PORT, HTTPServer, secure=False)
-http_server.start()
 
 
 class ReplayFunctionalityTest(TestCase):
@@ -87,7 +17,7 @@ class ReplayFunctionalityTest(TestCase):
         replaylib.reset()
 
     def _urlopen(self, path='', params=None):
-        return urllib.urlopen('http://localhost:%d/%s' % (PORT, path), params)
+        return urllib.urlopen('http://localhost:%d/%s' % (servers.PORT, path), params)
 
     def _grab(self, path='', params=None):
         webf = self._urlopen(path, params)
@@ -189,7 +119,7 @@ class ReplayFunctionalityTest(TestCase):
 
     def test_with_httplib(self):
         replaylib.start_record()
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.connect()
         conn.request("GET", "/")
         resp = conn.getresponse()
@@ -199,7 +129,7 @@ class ReplayFunctionalityTest(TestCase):
         data = replaylib.stop_record_obj()
 
         replaylib.start_playback_obj(data)
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.connect()
         conn.request("GET", "/")
         resp = conn.getresponse()
@@ -213,7 +143,7 @@ class ReplayFunctionalityTest(TestCase):
 
     def test_getheader_with_httplib(self):
         replaylib.start_record()
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.request("GET", "/")
         resp = conn.getresponse()
         real = resp.getheader('Content-type')
@@ -221,7 +151,7 @@ class ReplayFunctionalityTest(TestCase):
         data = replaylib.stop_record_obj()
 
         replaylib.start_playback_obj(data)
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.request("GET", "/")
         resp = conn.getresponse()
         fake = resp.getheader('Content-type')
@@ -244,7 +174,7 @@ class ReplayFunctionalityTest(TestCase):
 
     def test_read_partial(self):
         replaylib.start_record()
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.request("GET", "/")
         resp = conn.getresponse()
         real = resp.read(2)
@@ -252,7 +182,7 @@ class ReplayFunctionalityTest(TestCase):
         data = replaylib.stop_record_obj()
 
         replaylib.start_playback_obj(data)
-        conn = httplib.HTTPConnection('localhost:%d' % PORT)
+        conn = httplib.HTTPConnection('localhost:%d' % servers.PORT)
         conn.request("GET", "/")
         resp = conn.getresponse()
         fake = resp.read(2)
@@ -320,21 +250,7 @@ class ReplayStateTest(TestCase):
             raise AssertionError("record during playback should fail")
 
 
-if SSL:
-    class SecureHTTPServer(HTTPServer):
-        def __init__(self, server_address, HandlerClass):
-            BaseServer.__init__(self, server_address, HandlerClass)
-            ctx = SSL.Context(SSL.SSLv23_METHOD)
-            ctx.use_privatekey_file (TEST_CERT)
-            ctx.use_certificate_file(TEST_CERT)
-            self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
-                                                            self.socket_type))
-            self.server_bind()
-            self.server_activate()
-
-    https_server = ReferenceServer(SECURE_PORT, SecureHTTPServer, secure=True)
-    https_server.start()
-
+if servers.SSL:
     class ReplayFunctionalitySSLTest(TestCase):
 
         def tearDown(self):
@@ -342,13 +258,13 @@ if SSL:
 
         def test_single(self):
             replaylib.start_record()
-            webf = urllib.urlopen('https://localhost:%d' % SECURE_PORT)
+            webf = urllib.urlopen('https://localhost:%d' % servers.SECURE_PORT)
             real_compare = webf.read()
             webf.close()
             data = replaylib.stop_record_obj()
 
             replaylib.start_playback_obj(data)
-            webf = urllib.urlopen('https://localhost:%d' % SECURE_PORT)
+            webf = urllib.urlopen('https://localhost:%d' % servers.SECURE_PORT)
             fake_compare = webf.read()
             webf.close()
             replaylib.stop_playback()
---
+++ replaylib/tests/servers.py
@@ -0,0 +1,87 @@
+import os.path
+import socket
+
+from SocketServer import BaseServer
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+
+try:
+    from OpenSSL import SSL
+except ImportError:
+    SSL = None
+
+__here__ = os.path.dirname(__file__)
+
+# Port number to use for reference webserver.
+PORT = 9124
+SECURE_PORT = PORT + 1
+
+# Private key and certificate file to use for SSL testing.
+TEST_CERT = os.path.join(__here__, 'host.pem')
+
+
+class ReferenceServer(Thread):
+
+    def __init__(self, port, server_class, secure):
+        Thread.__init__(self)
+        self.port = port
+        self.counter = 0
+        self.server_class = server_class
+
+        class _Handler(BaseHTTPRequestHandler):
+
+            def do_GET(s):
+                self.counter += 1
+                s.send_response(200)
+                s.send_header('Content-type', 'text/plain')
+                s.end_headers()
+                resp = '%d requests to %s' % (self.counter, s.path)
+                s.wfile.write(resp)
+
+            def do_POST(s):
+                self.counter += 1
+                s.send_response(200)
+                s.send_header('Content-type', 'text/plain')
+                s.end_headers()
+                content_length = s.headers.getheader('Content-length')
+                if content_length:
+                    body = s.rfile.read(int(content_length))
+                else:
+                    body = ''
+                resp = '%d reqs to %s with %s' % (self.counter, s.path, body)
+                s.wfile.write(resp)
+
+            def log_message(s, *args):
+                pass
+
+            if secure:
+                def setup(s):
+                    s.connection = s.request
+                    s.rfile = socket._fileobject(s.request, 'rb', s.rbufsize)
+                    s.wfile = socket._fileobject(s.request, 'rb', s.wbufsize)
+
+        self.handler = _Handler
+
+    def run(self):
+        httpd = self.server_class(('localhost', self.port), self.handler)
+        httpd.serve_forever()
+
+
+http_server = ReferenceServer(PORT, HTTPServer, secure=False)
+http_server.start()
+
+
+if SSL:
+    class SecureHTTPServer(HTTPServer):
+        def __init__(self, server_address, HandlerClass):
+            BaseServer.__init__(self, server_address, HandlerClass)
+            ctx = SSL.Context(SSL.SSLv23_METHOD)
+            ctx.use_privatekey_file (TEST_CERT)
+            ctx.use_certificate_file(TEST_CERT)
+            self.socket = SSL.Connection(ctx, socket.socket(self.address_family,
+                                                            self.socket_type))
+            self.server_bind()
+            self.server_activate()
+
+    https_server = ReferenceServer(SECURE_PORT, SecureHTTPServer, secure=True)
+    https_server.start()
