b97cc1856b6bcec85a705e765b45f65b712d2484
Scott Torborg <scott@crookedmedia.com>
2010-06-15 20:18:05 -0700

    add pypi metadata fields to setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,5 @@
+import os.path
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -5,13 +7,26 @@ except ImportError:
     use_setuptools()
     from setuptools import setup, find_packages
 
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
            
 setup(name='replaylib',
       version='0.1',
+      author="Scott Torborg, Mike Spindel",
+      license="GPL",
+      keywords="testing web replay",
+      url="http://github.com/storborg/replaylib",
       description='Record and replay httplib actions for testing.',
       packages=find_packages(exclude=['ez_setup', 'tests']),
+      long_description=read('README.rst'),
       test_suite='nose.collector',
       zip_safe=False,
+      classifiers=[
+          "Development Status :: 3 - Alpha",
+          "License :: OSI Approved :: GNU General Public License (GPL)",
+          "Topic :: Software Development :: Testing"],
       entry_points="""
       [nose.plugins.0.10]
       replaylib = replaylib.noseplugin:ReplayLibPlugin
